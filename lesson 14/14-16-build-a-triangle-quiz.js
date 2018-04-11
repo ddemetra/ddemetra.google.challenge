9ee15edf08ba87a9ba3e79c1252d1993ddc19e5f
ddemetra <demetradmaria@gmail.com>
2018-04-11 04:05:39 -0400

    quiz

---
+++ lesson 14/14-16-build-a-triangle-quiz.js
@@ -0,0 +1,22 @@
+/*
+ * Programming Quiz: Build A Triangle (5-3)
+ */
+
+// creates a line of * for a given length
+
+function makeLine(length) {
+    var line = "";
+    for (var j = 1; j <= length; j++) {
+        line += "* ";
+    }
+    return line + "\n";
+}
+
+function buildTriangle(line) {
+	var triangle = "";
+	for (var i = 1; i <= line; i++) {
+		triangle += makeLine(i);
+	}
+
+	return triangle;
+}
