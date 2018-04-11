04e1f81f9efe6d270a97014cd9e9b5fd3d159919
ddemetra <demetradmaria@gmail.com>
2018-04-11 15:03:46 -0400

    quiz

---
+++ lesson 16/16-09-bank-accounts-1-quiz.js
@@ -0,0 +1,24 @@
+/*
+ * Programming Quiz: Bank Accounts 1 (7-3)
+ */
+
+var savingsAccount = {
+    balance: 1000,
+    interestRatePercent: 1,
+    deposit: function addMoney(amount) {
+        if (amount > 0) {
+            savingsAccount.balance += amount;
+        }
+    },
+    withdraw: function removeMoney(amount) {
+        var verifyBalance = savingsAccount.balance - amount;
+        if (amount > 0 && verifyBalance >= 0) {
+            savingsAccount.balance -= amount;
+        }
+    },
+    printAccountSummary: function (){
+        return 'Welcome!\n'+ 'Your balance is currently ' + '$' + this.balance + ' and your interest rate is ' + this.interestRatePercent + '%.';
+    }
+};
+
+console.log(savingsAccount.printAccountSummary());
