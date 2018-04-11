a38f78bbc3786c778554cb44ea29f2a9c8b05871
ddemetra <demetradmaria@gmail.com>
2018-04-11 05:15:21 -0400

    quiz

---
+++ lesson 15/15-07-crew-quiz.js
@@ -0,0 +1,15 @@
+/*
+ * Programming Quiz: Building the Crew (6-2)
+ */
+
+var captain = "Mal";
+var second = "Zoe";
+var pilot = "Wash";
+var companion = "Inara";
+var mercenary = "Jayne";
+var mechanic = "Kaylee";
+
+// your code goes here
+var crew = ["Mal", "Zoe", "Wash", "Inara", "Jayne", "Kaylee"];
+
+console.log(crew);
