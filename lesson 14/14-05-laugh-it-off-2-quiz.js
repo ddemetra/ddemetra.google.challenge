744e9ba4de1ce7c75c73b86cb4e59275c41106ef
ddemetra <demetradmaria@gmail.com>
2018-04-11 03:12:16 -0400

    quiz

---
+++ lesson 14/14-05-laugh-it-off-2-quiz.js
@@ -0,0 +1,21 @@
+/*
+ * Programming Quiz: Laugh it Off 2 (5-2)
+ *
+ * Write a function called `laugh` with a parameter named `num` that represents the number of "ha"s to return.
+ *
+ * Note:
+ *  - make sure your the final character is an exclamation mark ("!")
+ *  - make sure that your function produces the correct results when it is called multiple times
+ */
+
+function laugh(num) {
+    var laughs = "";
+     
+    for (var i = num; i > 0; i--) {
+        laughs += "ha";
+    }
+    
+    return laughs + "!";
+}
+
+console.log(laugh(10));
