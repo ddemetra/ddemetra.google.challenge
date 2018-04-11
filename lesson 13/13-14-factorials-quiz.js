6479ddfc161c903be849e57b62da976f57271846
ddemetra <demetradmaria@gmail.com>
2018-04-11 02:20:58 -0400

    quiz

---
+++ lesson 13/13-14-factorials-quiz.js
@@ -0,0 +1,10 @@
+/*
+ * Programming Quiz: Factorials (4-7)
+ */
+
+// your code goes here
+solution = 12;
+for (var fact = 1; fact < 12; fact ++) {
+    solution *= fact;
+}
+    console.log(solution);
