ed855b1819bd02dbba863728031812d01e916ba4
ddemetra <demetradmaria@gmail.com>
2018-04-11 05:21:34 -0400

    quiz

---
+++ lesson 15/15-08-the-price-is-right-quiz.js
@@ -0,0 +1,12 @@
+/*
+ * Programming Quiz: The Price is Right (6-3)
+ */
+
+var prices = [1.23, 48.11, 90.11, 8.50, 9.99, 1.00, 1.10, 67.00];
+
+// your code goes here
+prices [0] = 2.00;
+prices [3] = 9.00;
+prices [7] = 70.00;
+
+console.log(prices);
