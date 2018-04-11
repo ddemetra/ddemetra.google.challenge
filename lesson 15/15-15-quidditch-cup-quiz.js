1e9dab196392651df90fd53128024f795e9cea55
ddemetra <demetradmaria@gmail.com>
2018-04-11 05:48:33 -0400

    quiz

---
+++ lesson 15/15-15-quidditch-cup-quiz.js
@@ -0,0 +1,10 @@
+/*
+ * Programming Quiz: Quidditch Cup (6-5)
+ */
+
+function hasEnoughPlayers(team) {
+	return team.length >= 7;
+}
+
+var team = ["Oliver Wood", "Angelina Johnson", "Katie Bell", "Alicia Spinnet", "George Weasley", "Fred Weasley", "Harry Potter"];
+console.log(hasEnoughPlayers(team));
