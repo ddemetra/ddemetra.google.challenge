8868d9d4132799f70779c1ce5b26c044c32ab6da
ddemetra <demetradmaria@gmail.com>
2018-04-11 05:44:51 -0400

    quiz

---
+++ lesson 15/15-14-colors-of-the-rainbow-quiz.js
@@ -0,0 +1,9 @@
+/*
+ * Programming Quiz: Colors of the Rainbow (6-4)
+ */
+
+var rainbow = ["Red", "Orange", "Blackberry", "Blue"];
+
+// your code goes here
+
+var rainbow = ["Red", "Orange", "Yellow", "Green", "Purple", "Blue"];
